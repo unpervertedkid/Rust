5824d25acbb2508465471b273a2d3c795a42c7df
Brian Silah <unpervertedkid@gmail.com>
2022-11-01 20:08:58 +0300

    Destructing assignment

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let tuples1 = (1,2,3,"A string",3.5,("A nested tuple!", 30, 56));
+    let tuple2 = ("Rustacean", 21, 1);
+    let (name,age,years_of_experience) = tuple2;
 
     println!("{}",(tuples1.5).0);
+
+    println!("Name: {}, \nAge: {} \nYears of experience: {}",name,age,years_of_experience);
 }
