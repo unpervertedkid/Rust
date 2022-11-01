130841442a631c7130734a19c860f4f12188f869
Brian Silah <unpervertedkid@gmail.com>
2022-11-01 19:50:51 +0300

    If else statements

---
+++ if_else/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let age:i8 = 17;
+
+    if age > 18{
+        println!("You are old enough to be a rustacean");
+    }
+    else if age < 18{
+        println!("You are not too young to be a rustacean");
+    }
+    else{
+        println!("Are you an alien? Cause who doesnt have an age???");
+    }
+}
